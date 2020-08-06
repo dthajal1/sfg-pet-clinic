d42a490ef9616d6c88825311ff932c880b2046be
Diraj Thajali <diraj.thajali@berkeley.edu>
2020-08-06 12:47:29 -0700

    Implement Interfaces for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/com/clinic/sfgpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package com.clinic.sfgpetclinic.services;
+
+import com.clinic.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}

0e7983d9116c2ff34eef8526e0d5caa82f16b7e8
Diraj Thajali <diraj.thajali@berkeley.edu>
2020-08-17 13:34:56 -0700

    implemented find owners. Closes #54.

--- pet-clinic-data/src/main/java/com/clinic/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/clinic/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,8 +3,12 @@ package com.clinic.sfgpetclinic.repositories;
 import com.clinic.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/com/clinic/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/clinic/sfgpetclinic/services/OwnerService.java
@@ -2,8 +2,12 @@ package com.clinic.sfgpetclinic.services;
 
 import com.clinic.sfgpetclinic.model.Owner;
 
+import java.util.List;
+
 
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
